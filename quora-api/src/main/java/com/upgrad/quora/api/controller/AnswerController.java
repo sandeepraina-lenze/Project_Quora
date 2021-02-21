d260a2720cf349d59faf7519e7619f315c651463
Sarvjeet Rajvansh <srajvansh1124@gmail.com>
2021-02-22 01:00:41 +0530

    adding retrieve all answer for a question feature in AnswerController

--- quora-api/src/main/java/com/upgrad/quora/api/controller/AnswerController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/AnswerController.java
@@ -1,10 +1,12 @@
 package com.upgrad.quora.api.controller;
 
+import com.upgrad.quora.api.model.AnswerDetailsResponse;
 import com.upgrad.quora.api.model.AnswerRequest;
 import com.upgrad.quora.api.model.AnswerResponse;
 import com.upgrad.quora.service.business.AnswerBusinessService;
-import com.upgrad.quora.service.dao.QuestionDao;
+import com.upgrad.quora.service.dao.*;
 import com.upgrad.quora.service.entity.AnswerEntity;
+import com.upgrad.quora.service.entity.QuestionEntity;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.InvalidQuestionException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -20,19 +24,51 @@ import java.util.UUID;
 public class AnswerController {
     @Autowired
     private AnswerBusinessService answerBusinessService;
-
     @Autowired
     private QuestionDao questionDao;
+    @Autowired
+    private UserDao userDao;
+    @Autowired
+    private AnswerDao answerDao;
+    /** create Answer for a question with given question id in request
+     * @param questionId    id of the question to fetch the answers.
+     * @param authorization accessToken of the user for valid authentication.
+*/
 
-    @RequestMapping(method = RequestMethod.POST, path = "/question/{questionId}/answer/create", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<AnswerResponse> createAnswer(final AnswerRequest answerRequest, @RequestHeader("authorization") final String authorization, @PathVariable("questionId") final long questionId) throws InvalidQuestionException, AuthorizationFailedException {
+    @RequestMapping(method = RequestMethod.POST, path = "/question/{questionId}/answer/create", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<AnswerResponse> createAnswer(final AnswerRequest answerRequest, @RequestHeader("authorization") final String authorization, @PathVariable("questionId") final String questionId) throws InvalidQuestionException, AuthorizationFailedException {
+
         final AnswerEntity answerEntity = new AnswerEntity();
         answerEntity.setUuid(UUID.randomUUID().toString());
         answerEntity.setAns(answerRequest.getAnswer());
-
         final AnswerEntity createdAnswerEntity = answerBusinessService.createAnswer(answerEntity, authorization, questionId);
         AnswerResponse answerResponse =  new AnswerResponse().id(createdAnswerEntity.getUuid()).status("ANSWER CREATED");
 
         return new ResponseEntity<AnswerResponse>(answerResponse, HttpStatus.OK);
+    }
+    /**
+     * get all the answers for a question
+     *
+     * @param questionId    id of the question to fetch the answers.
+     * @param authorization accessToken of the user for valid authentication.
+     * @throws AuthorizationFailedException ATHR-001 - if User has not signed in. ATHR-002 if the User is signed out.
+     * @throws InvalidQuestionException      The question with entered uuid whose details are to be seen does not exist.
+     */
+    @RequestMapping(method = RequestMethod.GET, path = "/answer/all/{questionId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<List<AnswerDetailsResponse>> getAllAnswersToQuestion(@PathVariable("questionId") String questionId, @RequestHeader("authorization") final String authorization) throws AuthorizationFailedException, InvalidQuestionException {
+        List<AnswerEntity> answers = answerBusinessService.getAllAnswersToQuestion(questionId, authorization);
+        List<AnswerDetailsResponse> answerDetailsResponses = new ArrayList<>();
+
+        QuestionEntity questionEntity = questionDao.getQuestionById(questionId);
+        for (AnswerEntity answerEntity : answers) {
+            if(answerEntity.getQuestion().equals(questionDao.getQuestionById(questionId))){
+                AnswerDetailsResponse answerDetailsResponse = new AnswerDetailsResponse();
+                answerDetailsResponse.setId(answerEntity.getUuid());
+                answerDetailsResponse.setQuestionContent(answerEntity.getQuestion().getContent());
+                answerDetailsResponse.setAnswerContent(answerEntity.getAns());
+                answerDetailsResponses.add(answerDetailsResponse);
+            }
+        }
+        return new ResponseEntity<List<AnswerDetailsResponse>>(answerDetailsResponses, HttpStatus.OK);
     }
 }
--- quora-service/src/main/java/com/upgrad/quora/service/dao/AnswerDao.java
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/AnswerDao.java
@@ -5,13 +5,20 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public class AnswerDao {
     @PersistenceContext
     private EntityManager entityManager;
+// create Answer for a question
     public AnswerEntity createAnswer(AnswerEntity answerEntity) {
         entityManager.persist(answerEntity);
         return answerEntity;
+    }
+
+    //fetch all the answers to the question using questionId of question
+    public List<AnswerEntity> getAllAnswersToQuestion(final String questionId) {
+        return entityManager.createNamedQuery("getAllAnswersToQuestion", AnswerEntity.class).getResultList();
     }
 }
